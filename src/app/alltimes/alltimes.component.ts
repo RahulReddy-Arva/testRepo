7c28b3de07a35060029649071ecc81b48e312db1
RahulReddy-Arva <rahulreddy.arva55@gmail.com>
2017-10-06 20:06:24 -0400

    Adding new feature(time directly at run time and it should reflect in graphcool)

--- src/app/alltimes/alltimes.component.ts
+++ src/app/alltimes/alltimes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MenuItem, DataTable, LazyLoadEvent } from "primeng/primeng";
+import { MenuItem, DataTable, LazyLoadEvent, DialogModule } from "primeng/primeng";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import Dexie from 'dexie';
 import { Observable } from "rxjs";
 import { Apollo } from 'apollo-angular';
@@ -14,47 +15,97 @@ const MAX_EXAMPLE_RECORDS = 1000;
 })
 export class AlltimesComponent implements OnInit {
 
-@ViewChild("dt") dt : DataTable;
+  @ViewChild("dt") dt : DataTable;
 
-allTimesheetData = [];
+  allTimesheetData = [];
 
-allProjectNames = ['', 'Payroll App', 'Mobile App', 'Agile Times'];
+  addEntryForm: FormGroup;
 
-allProjects = this.allProjectNames.map((proj) => {
+  displayEntryForm = false;
+
+  allProjectNames = ['', 'Payroll App', 'Mobile App', 'Agile Times'];
+
+  allProjects = this.allProjectNames.map((proj) => {
     return { label: proj, value: proj }
   });
 
-selectedRows: Array<any>;
+  selectedRows: Array<any>;
 
-contextMenu: MenuItem[];
+  contextMenu: MenuItem[];
 
-recordCount : number;
+  recordCount : number;
 
-constructor(private apollo: Apollo) { }
+  display: boolean = false;
 
-ngOnInit() {
+  constructor(private apollo: Apollo, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.addEntryForm = this.fb.group({
+      User: ['', [Validators.required]],
+      Project: ['', [Validators.required]],
+      Category: ['', [Validators.required]],
+      StartTime: ['', [Validators.required]],
+      EndTime: ['', [Validators.required]]
+    })
+
     const AllClientsQuery = gql`
     query allTimesheets {
-    allTimesheets {
-    id
-    user
-    project
-    category
-    startTime
-    endTime
-    }
+      allTimesheets {
+          id
+          user
+          project
+          category
+          startTime
+          endTime
+        }
     }`;
+    
 
-  const queryObservable = this.apollo.watchQuery({
+    const queryObservable = this.apollo.watchQuery({
 
-    query: AllClientsQuery
+      query: AllClientsQuery,
+      pollInterval:200
+
     }).subscribe(({ data, loading }: any) => {
+
       this.allTimesheetData = data.allTimesheets;
       this.recordCount = data.allTimesheets.length;
 
-  });
+    });
+
   }
-  
-onEditComplete(editInfo) { }
+  onSaveComplete() {
+        const user = this.addEntryForm.value.User;
+        const project = this.addEntryForm.value.Project;
+        const category = this.addEntryForm.value.Category;
+        const startTime = this.addEntryForm.value.StartTime;
+        const endTime = this.addEntryForm.value.EndTime;
+    
+        const createTimesheet = gql`
+          mutation createTimesheet ($user: String!, $project: String!, $category: String!, $startTime: Int!, $endTime: Int!, $date: DateTime!) {
+            createTimesheet(user: $user, project: $project, category: $category, startTime: $startTime, endTime: $endTime, date: $date ) {
+              id
+            }
+          }
+        `;
+    
+        this.apollo.mutate({
+          mutation: createTimesheet,
+          variables: {
+            user: user,
+            project: project,
+            category: category,
+            startTime: startTime,
+            endTime: endTime,
+            date: new Date()
+          }
+        }).subscribe(({ data }) => {
+          console.log('got data', data);
+          
+        }, (error) => {
+          console.log('there was an error sending the query', error);
+        });
+        this.displayEntryForm = false;
+      }
 
 }
